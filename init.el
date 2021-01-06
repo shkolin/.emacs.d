e3f43bb32f7886b73ad882149ee2e34425931c8e
Vitalii Shkolin <vitalii@shkolin.net.ua>
2021-01-06 09:02:21 +0200

    Update init.el

--- init.el
+++ init.el
@@ -8,3 +8,9 @@
 ;; https://www.emacswiki.org/emacs/InteractivelyDoThings
 (require 'ido)
 (ido-mode t)
+
+;; https://orgmode.org/worg/org-tutorials/orgtutorial_dto.html
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
